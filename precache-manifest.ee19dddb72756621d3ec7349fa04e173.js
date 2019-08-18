89891b64015b935aaef697e99dd46399af4c62d2
Takaitech <takai.tech2144@gmail.com>
2019-08-18 13:18:53 -0700

    Updates

---
+++ precache-manifest.ee19dddb72756621d3ec7349fa04e173.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d973429d7f88d8462ce4e25acad4e2",
+    "url": "/reel-task/index.html"
+  },
+  {
+    "revision": "99a5b67982a791b8429a",
+    "url": "/reel-task/static/css/main.b7de0822.chunk.css"
+  },
+  {
+    "revision": "02be2796ef03f2cc4e97",
+    "url": "/reel-task/static/js/2.a5d4d979.chunk.js"
+  },
+  {
+    "revision": "99a5b67982a791b8429a",
+    "url": "/reel-task/static/js/main.8a51af11.chunk.js"
+  },
+  {
+    "revision": "2f135e2009a5e66a6d07",
+    "url": "/reel-task/static/js/runtime~main.0ea86550.js"
+  },
+  {
+    "revision": "c03a7621e3d3477601c64d0546d1572b",
+    "url": "/reel-task/static/media/Neou-Bold.c03a7621.ttf"
+  },
+  {
+    "revision": "f30f26198e4aac43f144359871c1e52b",
+    "url": "/reel-task/static/media/TOSTADA.f30f2619.ttf"
+  }
+]);
