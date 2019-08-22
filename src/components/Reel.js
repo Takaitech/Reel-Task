c2e5067def9c155e9290d57fa8eca20fc6d75298
Takaitech <takai.tech2144@gmail.com>
2019-08-22 15:04:13 -0700

    Fixed dropdown menu icon bug for mobile and added status icon to reels that changes on pause state change

--- src/components/Reel.js
+++ src/components/Reel.js
@@ -21,14 +21,16 @@ const Reel = ({brand, name, paused, percentSaved, total, url, toggleReelPause}) 
     return(
         <div className={styles.reelContainer}>
             <h2 className={styles.reelBrandName}>{brand}</h2>
-            <h3 className={styles.reelName}>{`${name.substring(0,20)}...`}</h3>
+            <h3 className={styles.reelName}>{`${name.substring(0,18)}...`}</h3>
             <div className={styles.reel}>
                 <span className={styles.reelPercent} >{percentSaved}%</span>
+                <div className={styles.reelImageOverlay}></div>
                 <img onClick={() => handleReelPause(name)} className={styles.reelImage} style={{border: `10px solid ${data.vibrant}` }} src={url}></img>
                 <div className={styles.progress} style={{height: `${percentSaved}%`, backgroundColor: data.vibrant, opacity: paused ? '0.3' : `1` }}>
             </div>
             </div>
             <span className={styles.totalPaid} >{paused ? 'Paused' :`STATUS:$${amountPaid(total,percentSaved)}/$${total}`}</span>
+            <img className={styles.statusIcon} src={paused ? 'icons/Pause-Icon.png': 'icons/Play-Icon.png' }></img>
         </div>
     )
 }
