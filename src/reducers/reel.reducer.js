a8c88df1f3ab4b5f65513b68251f118c88141213
Takaitech <takai.tech2144@gmail.com>
2019-08-22 13:15:00 -0700

    static images

--- src/reducers/reel.reducer.js
+++ src/reducers/reel.reducer.js
@@ -1,7 +1,7 @@
 let initialState = [
-    {brand: 'ACNE', name: 'Bryz Velcro-strap suede trainers', paused: false, percentSaved : 30, total: 1000, url: '/images/Acne-Placeholder.png'},
-    {brand: 'GUCCI', name: 'GG Marmont mini quilted-leather cross-body bag', paused: false, percentSaved : 50, total: 900, url: '/images/Gucci-Placeholder.jpg'},
-    {brand: 'FENDI ', name: 'X FILA Logo Sneakers', paused: false, percentSaved : 40, total: 250, url: '/images/Fendi-Placeholder.jpg'},
+    {brand: 'ACNE', name: 'Bryz Velcro-strap suede trainers', paused: false, percentSaved : 30, total: 1000, url: 'images/Acne-Placeholder.png'},
+    {brand: 'GUCCI', name: 'GG Marmont mini quilted-leather cross-body bag', paused: false, percentSaved : 50, total: 900, url: 'images/Gucci-Placeholder.jpg'},
+    {brand: 'FENDI ', name: 'X FILA Logo Sneakers', paused: false, percentSaved : 40, total: 250, url: 'images/Fendi-Placeholder.jpg'},
     {brand: 'FENDI', name: 'Mon Tresor perforated-logo leather bucket bag', paused: false, percentSaved : 25, total: 500, url: 'https://images.ctfassets.net/ljoz65y94szt/7E3wKXRFUOy8D0kEgRFodA/3f45dc8a37144d52136ae25ecce42af1/Isabel_Marant.jpg'},
     {brand: 'ALBUS LUMEN ', name: 'Safi striped single-breasted blazer', paused: false, percentSaved : 100, total: 1000, url: 'https://images.ctfassets.net/ljoz65y94szt/1eeYjBEsGcKzr7vZRBNLe0/01ca498a0918a68079844a1578f0d892/Gucci.jpg'},
     {brand: 'VETEMENTS ', name: 'Embroidered-logo denim cap', paused: false, percentSaved : 50, total: 1500, url: 'https://images.ctfassets.net/ljoz65y94szt/6V8mSNDehSzpxU1s5ylFZ5/401c5843ad19191257aef24b3bf03137/GRLFRND.jpg'},
